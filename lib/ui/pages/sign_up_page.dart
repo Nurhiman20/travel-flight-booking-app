93b39bac993fb8e24ba862d8308852f89d83fbc1
Angga Nurhiman <nurhimanangga@gmail.com>
2023-05-22 16:54:47 +0700

    feat: Reusable widget custom button

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -119,22 +120,11 @@ class SignUpPage extends StatelessWidget {
       }
 
       Widget submitButton() {
-        return SizedBox(
-          width: double.infinity,
-          height: 55,
-          child: TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/bonus');
-            },
-            style: TextButton.styleFrom(
-                backgroundColor: kPrimaryColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(defaultRadius))),
-            child: Text(
-              'Get Started',
-              style: whiteTextStyle.copyWith(fontSize: 18, fontWeight: medium),
-            ),
-          ),
+        return CustomButton(
+          title: 'Get Started',
+          onPressed: () {
+            Navigator.pushNamed(context, '/bonus');
+          },
         );
       }
 
