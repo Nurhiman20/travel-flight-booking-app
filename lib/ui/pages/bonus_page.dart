28eaed5c4fd4bdb143771706d2156f0d2ba4b789
Angga Nurhiman <nurhimanangga@gmail.com>
2023-05-22 16:27:40 +0700

    :sparkles: feat(Main page): Slicing

--- lib/ui/pages/bonus_page.dart
+++ lib/ui/pages/bonus_page.dart
@@ -97,7 +97,9 @@ class BonusPage extends StatelessWidget {
         height: 55,
         margin: const EdgeInsets.only(top: 50),
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pushNamed(context, '/main');
+          },
           style: TextButton.styleFrom(
               backgroundColor: kPrimaryColor,
               shape: RoundedRectangleBorder(
