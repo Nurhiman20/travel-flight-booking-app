e741c4f65acf4b4441647f8fb928477e2155afba
Angga Nurhiman <nurhimanangga@gmail.com>
2023-05-23 16:49:12 +0700

    feat(Home): Slicing header widget

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,136 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin:
+            EdgeInsets.only(left: defaultMargin, right: defaultMargin, top: 30),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Howdy,\nKezia Anne',
+                    style: blackTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Where to fly today?',
+                    style:
+                        greyTextStyle.copyWith(fontSize: 16, fontWeight: light),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image_profile.png'))),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget popularDestinations() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 323,
+              margin: EdgeInsets.only(left: defaultMargin),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18), color: kWhiteColor),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 180,
+                      height: 220,
+                      margin: const EdgeInsets.only(bottom: 20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          image: const DecorationImage(
+                              image:
+                                  AssetImage('assets/image_destination1.png'))),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 55,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: kWhiteColor,
+                              borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(18))),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                margin: const EdgeInsets.only(right: 2),
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icon_star.png'))),
+                              ),
+                              Text(
+                                '4.8',
+                                style:
+                                    blackTextStyle.copyWith(fontWeight: medium),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Lake Ciliwung',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 18, fontWeight: medium),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Tangerang',
+                            style: greyTextStyle.copyWith(fontWeight: light),
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [header(), popularDestinations()],
+    );
+  }
+}
